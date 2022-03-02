c6fcae527ad1b829f07cd4006e5b58181ec61660
Hugo Sacilotto <hugge.saci@gmail.com>
2022-03-02 17:26:27 +0100

    Working

--- src/main.rs
+++ src/main.rs
@@ -10,73 +10,54 @@ fn insertionsort(numbers: &mut Vec<isize>, start: usize, end: usize) {
             numbers[j] = numbers[j - 1];
             j -= 1;
         }
-
         numbers[j] = key;
     }
 }
 
-fn partition(numbers: &mut Vec<isize>, start: usize, end: usize) -> isize {
-    let pivot = numbers[end];
-    // println!("Pivot: {}", pivot);
-    // let mut i: isize = start as isize - 1;
-    // for j in start..end {
-    //     if numbers[j] < pivot {
-    //         i += 1;
-    //         numbers.swap(i as usize, j);
-    //     }
-    // }
-    // numbers.swap((i + 1) as usize, end);
-    // return i + 1;
-
-    let mut left = start;
-    let mut right = end;
-
+fn partition(numbers: &mut Vec<isize>, start: isize, end: isize) -> usize {
+    // Hoare's Partition Scheme using middle index as pivot
+    let pivot = numbers[((start + end) / 2) as usize];
+    let mut i = start - 1;
+    let mut j = end + 1;
 
     loop {
-        while left < right && numbers[left] <= pivot {
-            left += 1;
+        i += 1;
+        while numbers[i as usize] < pivot {
+            i += 1;
         }
-        while right > left && numbers[right] >= pivot {
-            right -= 1;
+        j -= 1;
+        while numbers[j as usize] > pivot {
+            j -= 1;
+        }
+        if i >= j {
+            return j as usize;
         }
 
-        if left < right {
-            numbers.swap(left, right);
-        }
-
-        if left >= right {
-            break;
-        }
+        numbers.swap(i as usize, j as usize);
     }
-
-    numbers.swap(left, end);
-
-    return left as isize;
 }
 
 fn quicksort(numbers: &mut Vec<isize>, start: isize, end: isize) {
     if start >= end { return; }
 
-    if end - start < 10 {
+    if end - start < 11 {   // Insertion sort on smaller arrays
         insertionsort(numbers, start as usize, end as usize);
         return;
     }
     
     // Get pivot index and rearrange
-    let pivot_index = partition(numbers, start as usize, end as usize);
-    // println!("Pivot index: {}", pivot_index);
-    // println!("{:?}\n", numbers);
+    let pivot_index = partition(numbers, start, end);
 
     // Left of pivot
-    quicksort(numbers, start, pivot_index - 1);
+    quicksort(numbers, start, (pivot_index) as isize);
 
     // Right of pivot
-    quicksort(numbers, pivot_index + 1, end);
+    quicksort(numbers, (pivot_index + 1) as isize, end);
 }
 
 fn main() {
     // Input
-    let mut line = String::with_capacity(2_000_000);
+    let mut line = String::with_capacity(3_000_000);
     io::stdin().lock().read_line(&mut line);
     
     let mut values: Vec<isize> = line
@@ -87,11 +68,6 @@ fn main() {
     
     let length = values.len() as isize;
 
-    // let mut values: Vec<isize> = vec![8, 7, 2, 1, 0, 9, 6];
-    // let length = values.len() as isize; 
-    // println!("Start: {:?}\n", values);
-
-    // Sort
     quicksort(&mut values, 0, length - 1);
 
     // Output
